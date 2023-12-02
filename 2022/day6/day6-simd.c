ff790eb8beb831e7776112b657e3c43416c4384d
P4C2UR <dpaczurb@gmail.com>
2023-12-02 20:53:50 +0100

    Make makefile also compile 2022

--- 2022/day6/day6-simd.c
+++ 2022/day6/day6-simd.c
@@ -16,13 +16,11 @@ int main(int argc, char **argv) {
   uint32_t* temp = malloc(sizeof(uint32_t)*8);
   uint32_t* last = malloc(sizeof(uint32_t)*8);
   uint32_t result=0;
-  unsigned int fsilver = 0;
-  __m128i values;
-  const __m128i mask = _mm_setr_epi32(-'a',-'a',-'a',-'a');
-  // const __m256i mask8 = _mm256_setr_epi32(-'a',-'a',-'a',-'a',-'a',-'a',-'a',-'a');
-  const __m128i one = _mm_setr_epi32(1,1,1,1);
-  //const __m256i one8 = _mm256_setr_epi32(1,1,1,1,1,1,1,1);
-  unsigned int i = 15;
+  uint fsilver = 0;
+  __m256i values;
+  const __m256i mask = _mm256_setr_epi32(-'a',-'a',-'a',-'a',-'a',-'a',-'a',-'a');
+  const __m256i one = _mm256_setr_epi32(1,1,1,1,1,1,1,1);
+  uint i = 15;
   int fd = open(argv[1], O_RDONLY);
   struct stat sb;
   fstat(fd,&sb);
@@ -31,34 +29,44 @@ int main(int argc, char **argv) {
                     PROT_READ,
                     MAP_PRIVATE,
                     fd,0);
-
-  values = _mm_setr_epi32(buff[0],buff[1],buff[2],buff[3]);
-  values = _mm_add_epi32(values,mask);
-  values = _mm_sllv_epi32(one,values);
-  _mm_storeu_si128((__m128i*) temp,values);
+  values = _mm256_setr_epi32(buff[0],buff[1],buff[2],buff[3],buff[4],buff[5],buff[6],buff[7]);
+  values = _mm256_add_epi32(values,mask);
+  values = _mm256_sllv_epi32(one,values);
+  _mm256_storeu_si256((__m256i*) temp,values);
   for(int j=0;j<4;j++) {
     last[j] = temp[j];
-    result ^= temp[j];
   }
-  for(;buff[i]!='\n';i+=4) {
-    values = _mm_setr_epi32(buff[i-3],buff[i-2],buff[i-1],buff[i]);
-    values = _mm_add_epi32(values,mask);
-    values = _mm_sllv_epi32(one,values);
-    _mm_storeu_si128((__m128i*) &temp[0],values);
+  for(int j=4;j<8;j++) {
+    result ^= temp[j];
+    last[j] = temp[j];
+  }
+
+
+  for(;buff[i]!='\n';i+=8) {
+    values = _mm256_setr_epi32(buff[i-7],buff[i-6],buff[i-5],buff[i-4],buff[i-3],buff[i-2],buff[i-1],buff[i]);
+    values = _mm256_add_epi32(values,mask);
+    values = _mm256_sllv_epi32(one,values);
+    _mm256_storeu_si256((__m256i*) &temp[0],values);
     for(int j=0;j<4;j++) {
-      result ^= last[j];
-      last[j] = temp[j];
+      result ^= last[j+4];
       result ^= temp[j];
       if(__builtin_popcount(result)==4) {
-        printf("%d\n",i-4+j);
-        goto silver_end;
+        printf("%d\n",i-6+j);
+        close(fd);
+        return 0;
+      }
+    }
+    for(int j=4;j<8;j++) {
+      result ^= temp[j-4];
+      result ^= temp[j];
+      last[j]=temp[j];
+      if(__builtin_popcount(result)==4) {
+        printf("%d\n",i-6+j);
+        close(fd);
+        return 0;
       }
     }
   }
- silver_end:
-  //TODO: second part
-  free(temp);
-  free(last);
   close(fd);
   return 0;
 }
--- Makefile
+++ Makefile
@@ -1,19 +1,29 @@
-BIN=2023/bin
+BIN23=2023/bin
+BIN22=2022/bin
 WARN=-Wall -Wextra
 DEBUG=-Og -ggdb3 -fsanitize=address -fsanitize=pointer-compare \
 -fsanitize=pointer-subtract -fsanitize=undefined \
 -fsanitize-address-use-after-scope -fstack-check \
 -fno-stack-clash-protection
-CFLAGS=$(DEBUG) $(WARN) -std=gnu99
+CFLAGS=$(DEBUG) $(WARN) -march=native -std=gnu99
 
-PROG=day1-part1 day1-part2 day2-part1 day2-part2
-LIST=$(addprefix $(BIN)/, $(PROG))
-INC=1
+PROG22=day3 day4 day5 day6 day6-simd day7
+PROG23=day1-part1 day1-part2 day2-part1 day2-part2
+LIST23=$(addprefix $(BIN23)/, $(PROG23))
+LIST22=$(addprefix $(BIN22)/, $(PROG22))
+INC23=1
+INC22=1
 
-all: $(LIST)
-$(VERBOSE).SILENT:
+all: $(LIST22) $(LIST23)
+#$(VERBOSE).SILENT:
 
-$(BIN)/%: $(wildcard 2023/*/*.c)
-		$(CC) $(word $(INC),$^) $(CFLAGS) -o $@
-		$(eval INC=$(shell bc <<<$(INC)+1))
+2023: $(LIST23)
+2022: $(LIST22)
 
+$(BIN23)/%: $(wildcard 2023/*/*.c)
+		$(CC) $(word $(INC23),$^) $(CFLAGS) -o $@
+		$(eval INC23=$(shell bc <<<$(INC23)+1))
+
+$(BIN22)/%: $(wildcard 2022/*/*.c)
+		$(CC) $(word $(INC22),$^) $(CFLAGS) -o $@
+		$(eval INC22=$(shell bc <<<$(INC22)+1))
