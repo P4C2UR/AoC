ae4ced794ebaf2f454e0c190fb2d63128f9e8507
P4C2UR <dpaczurb@gmail.com>
2023-12-02 14:17:39 +0100

    Add part1 made in js

---
+++ 2023/day2/part1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const readline = require('readline');
+
+var rd = readline.createInterface({
+  input: fs.createReadStream('2023/day2/data'),
+});
+let acc = 0
+rd.on('line',
+  line => {
+    const labeled = line.split(": ")
+    const number = parseInt(labeled[0].split(" ")[1])
+    const games = labeled[1].split("; ")
+    const takes = games.map(x=>x.split(", ").map(y=>y.split(" "))
+    .map(tuple=>(tuple[1][0]=='r')?tuple[0]<=12:(tuple[1][0]=='g')?tuple[0]<=13:tuple[0]<=14)
+    .reduce((acc,x)=>acc&&x,true))
+    .reduce((acc,x)=>acc&&x,true)
+    if(takes) acc += number;
+  })
+rd.on('close',
+  ()=>console.log(acc))
