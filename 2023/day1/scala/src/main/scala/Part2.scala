ced675cad5f077ba0af193be483ba254429cd062
P4C2UR <dpaczurb@gmail.com>
2023-12-02 01:06:44 +0100

    Add broken solution for Part2 in scala

---
+++ 2023/day1/scala/src/main/scala/Part2.scala
@@ -0,0 +1,30 @@
+package main.scala
+import scala.io.Source._
+
+
+/*
+ * Not working because exercise requires you
+ * to parse some numbers starting at the back and
+ * some starting at the front which in turn
+ * requires parsing it manually
+ * It's too much work
+ */
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    val lines = fromFile("../data").getLines.drop(1)
+      .map(line=>line
+      .replace("one",   "1")
+      .replace("two",   "2")
+      .replace("three", "3")
+      .replace("four",  "4")
+      .replace("five",  "5")
+      .replace("six",   "6")
+      .replace("seven", "7")
+      .replace("eight", "8")
+      .replace("nine",  "9")
+      .filter(_.isDigit).toString)
+      .foldLeft(0)((acc,digits)=>acc+digits(0).asDigit*10+digits(digits.length-1).asDigit)
+    println(lines)
+  }
+}
