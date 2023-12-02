bf20ee1b1e06a0d9a4ad32cc9e243231ef2063a9
P4C2UR <dpaczurb@gmail.com>
2023-12-02 01:42:00 +0100

    Add excuse for not making part2 in node.js

---
+++ 2023/day1/part2.js
@@ -0,0 +1,7 @@
+/*
+ * Missing because exercise requires you
+ * to parse some numbers starting at the back and
+ * some starting at the front which in turn
+ * requires parsing it manually
+ * It's too much work
+ */
