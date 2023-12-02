610c07dcc7071be4674fd7c062ee9a0d69397530
P4C2UR <dpaczurb@gmail.com>
2023-12-02 01:40:54 +0100

    Add part1 solution in node.js

---
+++ 2023/day1/part1.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const readline = require('readline');
+
+function asDigit(x) {
+  return x.charCodeAt(0)-'0'.charCodeAt(0)
+}
+
+var rd = readline.createInterface({
+  input: fs.createReadStream('2023/day1/data'),
+});
+var acc = 0;
+rd.on('line',
+  line => {
+    if(line.length==0) return;
+    const chars = Array.from(line).filter(char=>char>='0'&&char<='9');
+    acc += asDigit(chars[0])*10+asDigit(chars[chars.length-1])
+  })
+rd.on('close', ()=>console.log(acc))
