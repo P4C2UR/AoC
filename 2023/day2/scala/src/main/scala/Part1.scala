75e0989aafa223ae76374c35431edb36f715e8a8
P4C2UR <dpaczurb@gmail.com>
2023-12-02 13:55:17 +0100

    Make part1 in scala more obfuscated

--- 2023/day2/scala/src/main/scala/Part1.scala
+++ 2023/day2/scala/src/main/scala/Part1.scala
@@ -3,14 +3,6 @@ import scala.io.Source._
 
 object Part1 {
   def check(game: List[(Int, Char)]): Boolean = {
-    game.map(x=>
-        x(1) match
-        case 'r' => x(0)<=12
-        case 'g' => x(0)<=13
-        case 'b' => x(0)<=14)
-    .foldLeft(true)((acc,x)=>acc&&x)
-  }
-
   def main(args: Array[String]): Unit = {
     val lines = fromFile("../data").getLines
       .map(line=>line.split(": "))
@@ -22,7 +14,14 @@ object Part1 {
             val s = color.split(" ")
             (s(0).toInt,s(1)(0))
           })).flatten))
-      .filter(x=>check(x(1)))
+      .filter(x=>{
+        x(1).map(x=>
+        x(1) match
+        case 'r' => x(0)<=12
+        case 'g' => x(0)<=13
+        case 'b' => x(0)<=14)
+        .foldLeft(true)((acc,x)=>acc&&x)
+      })
       .foldLeft(0)((acc,x)=>acc+x(0))
     println(lines)
   }
