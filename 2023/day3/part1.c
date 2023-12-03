bf9bb3d19dec3debc191e8793322ef790b06374e
P4C2UR <dpaczurb@gmail.com>
2023-12-03 11:42:00 +0100

    Switch names of alt and normal version of day3 C

--- 2023/day3/part1.c
+++ 2023/day3/part1.c
@@ -7,10 +7,10 @@
 
 size_t max_length = LINE_SIZE;
 FILE *f;
-char *line;
-char *pline;
+char **lines;
 int length;
 int acc = 0;
+char *t;
 
 int parseNumber(char *arr, int i) {
   int ret = 0;
@@ -35,40 +35,40 @@ int main(int argc, char** argv) {
     puts("Pass path to data file as argument");
     return 1;
   }
-  line = malloc(sizeof(char)*LINE_SIZE);
-  pline = malloc(sizeof(char)*LINE_SIZE);
-  for(int i=0;i<LINE_SIZE;i++)
-    pline[i]='.';
+  lines = malloc(sizeof(char*)*3);
+  for(int i=0;i<3;i++)
+    lines[i] = malloc(sizeof(char)*LINE_SIZE);
+  for(int i=0;i<LINE_SIZE;i++) {
+    lines[0][i]='.';
+    lines[1][i]='.';
+  }
   f = fopen(argv[1], "r");
-  for(length = getline(&line,&max_length, f);
-      length>0;
-      memcpy(pline,line,max_length),
-      length = getline(&line,&max_length, f)) {
+  for(length = getline(&lines[2],&max_length, f); length>0;) {
     for(int i=0;i<length-1;i++) {
-      if(isDigit(line[i])) {
-        if(i-1>-1 && !isDigit(pline[i-1]) && pline[i-1]!='.') {
-          acc += parseNumber(line,i);
-        } else if(!isDigit(pline[i]) && pline[i]!='.') {
-          acc += parseNumber(line,i);
-        } else if(i+1<length-1 && !isDigit(pline[i+1]) && pline[i+1]!='.') {
-          acc += parseNumber(line,i);
+      if(!isDigit(lines[1][i]) && lines[1][i]!='.') {
+        if(i-1>=0) {
+          for(int j=0;j<3;j++) {
+            if(isDigit(lines[j][i-1]))
+              acc += parseNumber(lines[j], i-1);
+          }
         }
-      } else if(line[i]!='.') {
-        if(i-1>-1 && isDigit(pline[i-1]))
-          acc += parseNumber(pline, i-1);
-        if(isDigit(pline[i]))
-          acc += parseNumber(pline, i);
-        if(i+1<length-1 && isDigit(pline[i+1]))
-          acc += parseNumber(pline, i+1);
-        if(isDigit(line[i-1]))
-          acc += parseNumber(line, i-1);
-        if(isDigit(line[i+1]))
-          acc += parseNumber(line, i+1);
+        if(isDigit(lines[0][i]))
+          acc += parseNumber(lines[0],i);
+        if(isDigit(lines[2][i]))
+          acc += parseNumber(lines[2],i);
+        if(i+1<length-1) {
+          for(int j=0;j<3;j++) {
+            if(isDigit(lines[j][i+1]))
+              acc += parseNumber(lines[j], i+1);
+          }
+        }
       }
     }
-    for(int i=0;i<length;i++)
-      printf("%c",pline[i]);
-    puts("");
+    t=lines[0];
+    lines[0] = lines[1];
+    lines[1] = lines[2];
+    lines[2] = t;
+    length = getline(&lines[2],&max_length, f);
   }
   printf("%d\n", acc);
   return 0;
