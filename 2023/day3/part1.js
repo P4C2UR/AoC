32a2fa3eaff3f21ab8ebcecdbb238c44f54d726a
P4C2UR <dpaczurb@gmail.com>
2023-12-04 13:17:14 +0100

    Finnish part1 day3 in js

--- 2023/day3/part1.js
+++ 2023/day3/part1.js
@@ -11,14 +11,28 @@ var rd = readline.createInterface({
 });
 let prev = ""
 let curr = ""
+let acc = 0;
 
 function isDigit(x) { return x>='0'&&x<='9' }
 function parseNumber(arr, i) {
-  console.log(arr.charAt(i))
+  let ret = 0;
+  while(isDigit(arr[i])) {
+    i--
+    if(i==-1) break;
+  }
+  i++;
+  while(isDigit(arr[i])) {
+    ret*=10;
+    ret+=arr[i]-'0';
+    arr[i] = '.';
+    i++;
+  }
+  return ret
 }
 
 rd.on('line',
   line => {
+    line = Array.from(line)
     if(prev=="") {
       prev = line
       return
@@ -27,27 +41,53 @@ rd.on('line',
       return
     }
     for(let i=0;i<line.length;i++) {
-      if(!isDigit(line[i]) && line[i]!='.') {
+      if(!isDigit(curr[i]) && curr[i]!='.') {
         if(i>0) {
-          if(isDigit(prev.charAt(i-1)))
-            parseNumber(prev, i-1)
-          if(isDigit(curr.charAt(i-1)))
-            parseNumber(curr, i-1)
-          if(isDigit(line.charAt(i-1)))
-            parseNumber(line, i-1)
+          if(isDigit(prev[i-1]))
+            acc += parseNumber(prev, i-1)
+          if(isDigit(curr[i-1]))
+            acc += parseNumber(curr, i-1)
+          if(isDigit(line[i-1]))
+            acc += parseNumber(line, i-1)
         }
-        if(isDigit(prev.charAt(i)))
-          parseNumber(prev, i)
-        if(isDigit(line.charAt(i)))
-          parseNumber(line, i)
+        if(isDigit(prev[i]))
+          acc += parseNumber(prev, i)
+        if(isDigit(line[i]))
+          acc += parseNumber(line, i)
         if(i+1<line.length) {
-          if(isDigit(prev.charAt(i+1)))
-            parseNumber(prev, i+1)
-          if(isDigit(curr.charAt(i+1)))
-            parseNumber(curr, i+1)
-          if(isDigit(line.charAt(i+1)))
-            parseNumber(line, i+1)
+          if(isDigit(prev[i+1]))
+            acc += parseNumber(prev, i+1)
+          if(isDigit(curr[i+1]))
+            acc += parseNumber(curr, i+1)
+          if(isDigit(line[i+1]))
+            acc += parseNumber(line, i+1)
         }
       }
     }
+    prev = curr;
+    curr = line;
   })
+
+rd.on('close', ()=> {
+  for(let i=0;i<curr.length;i++) {
+    if(!isDigit(curr[i]) && curr[i]!='.') {
+      if(i>0) {
+        if(isDigit(prev[i-1]))
+          acc += parseNumber(prev, i-1)
+        if(isDigit(curr[i-1]))
+          acc += parseNumber(curr, i-1)
+      }
+      if(isDigit(prev[i]))
+        acc += parseNumber(prev, i)
+      if(isDigit(curr[i]))
+        acc += parseNumber(curr, i)
+      if(i+1<curr.length) {
+        if(isDigit(prev[i+1]))
+          acc += parseNumber(prev, i+1)
+        if(isDigit(curr[i+1]))
+          acc += parseNumber(curr, i+1)
+      }
+    }
+  }
+  console.log(acc)
+})
